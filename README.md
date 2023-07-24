8c2389c58a086bebadfd273ac2e9d0eb104f0ce6
Vishal Kharade <vishalthedeveloper07@gmail.com>
2023-07-24 22:00:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# vue3-reactive-api
+
+This template should help get you started developing with Vue 3 in Vite.
+
+# Purpose
+The purpose of this project is to show the use of 'reactive' api in vue 3.
+
+# Prerequisites
+
+This project runs on Node version 18.16.0 and npm version 9.5.1 and Vue js 3
+
+# How this project is created
+
+Run command
+```sh
+npm init vue@latest
+```
+Follow the sequence of commands to create the project.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { reactive } from 'vue';
+
+const state = reactive({data:'testing', number:20})
+
+function changeData(){
+  state.data = 'changed data';
+}
+
+function changeNumber(){
+  state.number = 30;
+}
+</script>
+
+<template>
+  <div class="container">
+   <p>Reactive state object's data</p>
+   <p> {{ state.data }}</p>
+   <p> {{ state.number }}</p>
+   <button class="btnData" @click="changeData">Change Data</button>
+   <button class="btnData" @click="changeNumber">Change Number</button>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  width:100%;
+  border: 2px solid white;
+}
+.btnData{
+  margin: 10px;
+  width: 150px;
+  height: 30px;
+  border: 1ps solid black;
+  border-radius: 8px;
+}
+</style>
